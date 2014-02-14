6668c7f7e4b113d5d5e04d49b47abe0920951739
Chris Jordan <jordanchriss@gmail.com>
2014-02-14 01:02:12 -0500

    Switched to glut for homework

--- LearnOpenGL/LearnOpenGL/main.cpp
+++ LearnOpenGL/LearnOpenGL/main.cpp
@@ -1,153 +1,128 @@
-//#define GLEW_STATIC //what is static vs dynamic, why is this needed when static?
-#include <GL/glew.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_opengl.h>
+/* Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+/* File for "Basic Shapes" lesson of the OpenGL tutorial on
+ * www.videotutorialsrock.com
+ */
 
-// Shader sources
-const GLchar* vertexSource =
-"#version 150 core\n"
-"in vec2 position;"
-"in vec3 color;"
-"out vec3 Color;"
-"void main() {"
-"    Color = color;"
-"    gl_Position = vec4(position, 0.0, 1.0);"
-"}";
-const GLchar* fragmentSource =
-"#version 150 core\n"
-"in vec3 Color;"
-"out vec4 outColor;"
-"void main() {"
-"    outColor = vec4(Color, 1.0);"
-"}";
 
-void printGLError(const char *prefix) {
-    fprintf (stderr, "%s - OpenGL Error: %s\n", prefix, gluErrorString(glGetError()));
+
+#include <iostream>
+#include <stdlib.h> //Needed for "exit" function
+
+//Include OpenGL header files, so that we can use OpenGL
+#ifdef __APPLE__
+#include <OpenGL/OpenGL.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+using namespace std;
+
+//Called when a key is pressed
+void handleKeypress(unsigned char key, //The key that was pressed
+					int x, int y) {    //The current mouse coordinates
+	switch (key) {
+		case 27: //Escape key
+			exit(0); //Exit the program
+	}
 }
 
-int main(int argc, char *argv[]) {
-    SDL_Init(SDL_INIT_VIDEO);
+//Initializes 3D rendering
+void initRendering() {
+	//Makes 3D drawing work when something is in front of something else
+	glEnable(GL_DEPTH_TEST);
+}
 
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+//Called when the window is resized
+void handleResize(int w, int h) {
+	//Tell OpenGL how to convert from coordinates to pixel values
+	glViewport(0, 0, w, h);
 
-    SDL_Window* window = SDL_CreateWindow("OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
-    SDL_GLContext context = SDL_GL_CreateContext(window);
+	glMatrixMode(GL_PROJECTION); //Switch to setting the camera perspective
 
+	//Set the camera perspective
+	glLoadIdentity(); //Reset the camera
+	gluPerspective(45.0,                  //The camera angle
+				   (double)w / (double)h, //The width-to-height ratio
+				   1.0,                   //The near z clipping coordinate
+				   200.0);                //The far z clipping coordinate
+}
 
-    printGLError("setattributes");
+//Draws the 3D scene
+void drawScene() {
+	//Clear information from last draw
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Initialize GLEW
-    glewExperimental = GL_TRUE;
-    glewInit();
+	glMatrixMode(GL_MODELVIEW); //Switch to the drawing perspective
+	glLoadIdentity(); //Reset the drawing perspective
 
-    printGLError("glewInit");
+	glBegin(GL_QUADS); //Begin quadrilateral coordinates
 
-    // Create and compile the vertex shader
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexSource, NULL);
-    glCompileShader(vertexShader);
+	//Trapezoid
+	glVertex3f(-0.7f, -1.5f, -5.0f);
+	glVertex3f(0.7f, -1.5f, -5.0f);
+	glVertex3f(0.4f, -0.5f, -5.0f);
+	glVertex3f(-0.4f, -0.5f, -5.0f);
 
-    GLint status;
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+	glEnd(); //End quadrilateral coordinates
 
-    if (status != GL_TRUE) {
-        char buffer[512];
-        glGetShaderInfoLog(vertexShader, 512, NULL, buffer);
-        exit(4);
-    }
+	glBegin(GL_TRIANGLES); //Begin triangle coordinates
 
-    // Create and compile the fragment shader
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
-    glCompileShader(fragmentShader);
+	//Pentagon
+	glVertex3f(0.5f, 0.5f, -5.0f);
+	glVertex3f(1.5f, 0.5f, -5.0f);
+	glVertex3f(0.5f, 1.0f, -5.0f);
 
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
+	glVertex3f(0.5f, 1.0f, -5.0f);
+	glVertex3f(1.5f, 0.5f, -5.0f);
+	glVertex3f(1.5f, 1.0f, -5.0f);
 
-    if (status != GL_TRUE) {
-        exit(5);
-    }
+	glVertex3f(0.5f, 1.0f, -5.0f);
+	glVertex3f(1.5f, 1.0f, -5.0f);
+	glVertex3f(1.0f, 1.5f, -5.0f);
 
-    printGLError("fragmentShader");
+	//Triangle
+	glVertex3f(-0.5f, 0.5f, -5.0f);
+	glVertex3f(-1.0f, 1.5f, -5.0f);
+	glVertex3f(-1.5f, 0.5f, -5.0f);
 
-    // Create Vertex Array Object
-    GLuint vao;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
+	glEnd(); //End triangle coordinates
 
+	glutSwapBuffers(); //Send the 3D scene to the screen
+}
 
-    // Create a Vertex Buffer Object and copy the vertex data to it
-    GLuint vbo;
-    glGenBuffers(1, &vbo);
+int main(int argc, char** argv) {
+	//Initialize GLUT
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(400, 400); //Set the window size
 
-    float vertices[] = {
-        -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // Top-left
-        0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // Top-right
-        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
-        -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, // Bottom-left
-    };
+	//Create the window
+	glutCreateWindow("Basic Shapes - videotutorialsrock.com");
+	initRendering(); //Initialize rendering
 
-    GLuint elements[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
+	//Set handler functions for drawing, keypresses, and window resizes
+	glutDisplayFunc(drawScene);
+	glutKeyboardFunc(handleKeypress);
+	glutReshapeFunc(handleResize);
 
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    printGLError("VBO");
-
-    // Link the vertex and fragment shader into a shader program
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glBindFragDataLocation(shaderProgram, 0, "outColor");
-    glLinkProgram(shaderProgram);
-    glUseProgram(shaderProgram);
-
-    printGLError("shaderProgram");
-
-    // Specify the layout of the vertex data
-    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
-    printGLError("posAttrib");
-    glEnableVertexAttribArray(posAttrib);
-    printGLError("glEnableVertexAttribArray");
-    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), 0);
-    printGLError("glVertexAttribPointer");
-
-    GLint colAtrib = glGetAttribLocation(shaderProgram, "color");
-    glEnableVertexAttribArray(colAtrib);
-    glVertexAttribPointer(colAtrib, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)(2*sizeof(float)));
-
-
-    GLuint ebo;
-    glGenBuffers(1, &ebo);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-                 sizeof(elements), elements, GL_STATIC_DRAW);
-
-    SDL_Event windowEvent;
-    while (true) {
-        if (SDL_PollEvent(&windowEvent)) {
-            if (windowEvent.type == SDL_QUIT) break;
-        }
-
-        // Clear the screen to black
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-        // Swap buffers
-        SDL_GL_SwapWindow(window);
-    }
-
-    SDL_GL_DeleteContext(context);
-    SDL_Quit();
-    return 0;
+	glutMainLoop(); //Start the main loop.  glutMainLoop doesn't return.
+	return 0; //This line is never reached
 }
